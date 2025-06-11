121a4ab4ca94fb186af98e895feae66ae8ef6771
DawsonD88 <dawsond2554@student.faytechcc.edu>
2025-06-11 15:40:55 -0400

    First Submission

---
+++ P1LAB1_DawsonDeiontay.py
@@ -0,0 +1,11 @@
+"""
+Deiontay Dawson
+11 June 2025
+P1LAB1_CTI110
+Demonstrating Basic Input/Output using Python
+"""
+
+FirstName= input("Enter First Name:")
+LastName= input("Enter Last Name:")
+
+print("Hello,",FirstName,LastName,"!","Welcome to CTI-110")
