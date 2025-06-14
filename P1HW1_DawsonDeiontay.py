6f2b0438a9a90495365cf6722793f9d78a63b0ed
DawsonD88 <dawsond2554@student.faytechcc.edu>
2025-06-14 15:20:29 +0000

    P1HW1 initial commit

---
+++ P1HW1_DawsonDeiontay.py
@@ -0,0 +1,35 @@
+# Deiontay Dawson
+# 14 June 2025
+# P1HW1_DawsonDeiontay
+# Demonstrate the collection and processing of inputted information from users and display it for the users.
+
+print("-----Calculating Exponenets----")
+print()
+print()
+print("Enter an integer as the base value:",end="")
+basevalue=int(input())
+print("Enter an integer as the exponent:",end="")
+exponent=int(input())
+solution=int(basevalue**exponent)
+print()
+print()
+print(basevalue,"raised to the power of", exponent,"is", solution, "!!")
+print()
+print()
+print("-----Addition and Subtraction----")
+print()
+print()
+print("Enter a starting integer:", end="")
+x=int(input())
+print("Enter an integer to add:", end="")
+y=int(input())
+print("Enter an integer to subtract:", end="")
+z=int(input())
+ans=int(x + y - z)
+print()
+print()
+print(x,"+", y,"-", z, "is equal to", ans)
+
+
+
+
